d52d452299d5ebdd104b7c623b104c14bdb86ffb
Florian Nunez <fnunez@splio.com>
2023-12-13 18:17:06 +0100

    one day one game !

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,20 @@
-import { Events } from "discord.js";
-
+import { Events } from 'discord.js';
+import cron from 'node-cron';
+import { isProd } from '../lib/utils.js';
+import { oneDayOneGame } from '../lib/cronTasks.js';
 export const name = Events.ClientReady;
 export const once = true;
-export const execute = (client) =>
-  console.log(`Ready ! Logged in as ${client.user.tag}`);
+
+const oneDayOneGameSchedule = isProd ? '0 7 * * *' : '* * * * *';
+
+export const execute = async (client) => {
+    try {
+        console.log(`Ready ! Logged in as ${client.user.tag}`);
+        const oneDayOneGameCron = cron.schedule(oneDayOneGameSchedule, () =>
+            oneDayOneGame(client)
+        );
+        oneDayOneGameCron.start();
+    } catch (error) {
+        console.log('Ready Event - error ', error);
+    }
+};
